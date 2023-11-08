d4bcc91a61add0c1de784cb96ae6bb0f3682fd5c
Harr-is <zbinnawaz@gmail.com>
2023-11-09 00:21:03 +0500

    Delete Function

--- script.js
+++ script.js
@@ -42,3 +42,17 @@ function loadRecipies() {
       },
     });
   }
+
+  function handleDelete() {
+    var btn = $(this);
+    var parentDiv = btn.closest(".recipe");
+    let id = parentDiv.attr("data-id");
+    console.log(id);
+    $.ajax({
+      url: "https://usman-fake-api.herokuapp.com/api/recipes/" + id,
+      method: "DELETE",
+      success: function () {
+        loadRecipies();
+      },
+    });
+  }
